eb43a586d0e50c52ef5f4b99d62e9f41a5be4ef3
Puff82922 <skirchner@csumb.edu>
2024-03-18 07:22:53 -0700

    Instruction view of sections teaching for email=dwisneski@csumb.edu

--- src/components/instructor/InstructorSectionsView.js
+++ src/components/instructor/InstructorSectionsView.js
@@ -1,22 +1,80 @@
+import {SERVER_URL} from '../../Constants';
 import React, {useState, useEffect} from 'react';
+import {Link, useLocation} from "react-router-dom";
 
-// instructor views a list of sections they are teaching 
+
+// instructor views a list of sections they are teaching
 // use the URL /sections?email=dwisneski@csumb.edu&year= &semester=
 // the email= will be removed in assignment 7 login security
 // The REST api returns a list of SectionDTO objects
 // The table of sections contains columns
 //   section no, course id, section id, building, room, times and links to assignments and enrollments
-// hint:  
+// hint:
 // <Link to="/enrollments" state={section}>View Enrollments</Link>
 // <Link to="/assignments" state={section}>View Assignments</Link>
 
 const InstructorSectionsView = (props) => {
+    const headers = ['Section No.', 'Course ID','Section ID', 'Building', 'Room', 'Times', '', '']
 
-    
-     
+    const location = useLocation();
+    const { year, semester } = location.state;
+
+    const [ sections, setSections ] = useState([]);
+
+    const [ message, setMessage ] = useState('');
+
+    const fetchCourses = async () => {
+        try{
+            const response = await fetch(`${SERVER_URL}/sections?email=dwisneski@csumb.edu&year=${year}&semester=${semester}`);
+            if (response.ok){
+                const sections = await response.json();
+                setSections(sections);
+            } else {
+                const json = await response.json();
+                setMessage("response error: "+ json.message)
+            }
+        } catch (err) {
+            setMessage("network error: "+ err);
+        }
+    }
+
+    useEffect(() => {
+        fetchCourses();
+    }, [year, semester])
+
+
+
     return(
-        <> 
-           <h3>Not implemented</h3>
+        <>
+            <h3>Sections</h3>
+            <h4>{message}</h4>
+            <table className="Center">
+                <thead>
+                <tr>
+                    {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                </tr>
+                </thead>
+                <tbody>
+                {sections.map((section) => (
+                    <tr key = {section.secId}>
+                        <td>{section.secNo}</td>
+                        <td>{section.courseId}</td>
+                        <td>{section.secId}</td>
+                        <td>{section.building}</td>
+                        <td>{section.room}</td>
+                        <td>{section.times}</td>
+                        <td>
+                            <Link to="/assignments" state={section}>View Assignments</Link>
+                        </td>
+                        <td>
+                            <Link to="/enrollments" state={section}>View Enrollments</Link>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+
+            </table>
+
         </>
     );
 }
