63460f4b1e173a692d14ee2f551e94f40c7d64d0
Puff82922 <skirchner@csumb.edu>
2024-03-18 06:55:39 -0700

    View list of students enrolled in a section and update their grade

--- src/components/instructor/EnrollmentsView.js
+++ src/components/instructor/EnrollmentsView.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
+import {SERVER_URL} from "../../Constants";
+import Button from "@mui/material/Button";
+import DialogActions from "@mui/material/DialogActions";
 
-// instructor view list of students enrolled in a section 
+// instructor view list of students enrolled in a section
 // use location to get section no passed from InstructorSectionsView
 // fetch the enrollments using URL /sections/{secNo}/enrollments
 // display table with columns
@@ -11,12 +14,86 @@ import {useLocation} from 'react-router-dom';
 
 const EnrollmentsView = (props) => {
 
+    const headers = ['Enrollment ID', 'Student ID', 'Name', 'Email', 'Grade']
     const location = useLocation();
-    const {secNo, courseId, secId} = location.state;
+    const {secNo} = location.state;
+    const [enrollments, setEnrollments] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const fetchEnrollments = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/sections/${secNo}/enrollments`);
+            if (response.ok) {
+                const enrollments = await response.json();
+                setEnrollments(enrollments);
+            } else {
+                const json = await response.json();
+                setMessage("response error: " + json.message)
+            }
+        } catch (err) {
+            setMessage("network error: " + err);
+        }
+    }
+
+    useEffect(() => {
+        fetchEnrollments();
+    }, [secNo])
+
+    const onGradeChange = (event, enrollmentId) => {
+        setEnrollments(enrollments.map(enrollment =>
+            enrollment.enrollmentId === enrollmentId ? {...enrollment, grade: event.target.value} : enrollment));
+    }
+
+    const saveEnrollment = async (enrollments) => {
+        try {
+            const response = await fetch (`${SERVER_URL}/enrollments`,
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(enrollments),
+                });
+            if (response.ok) {
+                setMessage("Enrollment saved");
+            } else {
+                const rc = await response.json();
+                setMessage(rc.message);
+            }
+        } catch (err) {
+            setMessage("network error: "+err);
+        }
+    }
+
+    const onSave = async () => {
+        saveEnrollment(enrollments);
+    }
 
     return(
-        <> 
-            <h3>Not implemented</h3>   
+        <>
+            <h3>Enrollments</h3>
+            <h4>{message}</h4>
+            <table className="Center">
+                <thead>
+                <tr>
+                    {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                </tr>
+                </thead>
+                <tbody>
+                {enrollments.map((e) => (
+                    <tr key = {e.enrollmentId}>
+                        <td>{e.enrollmentId}</td>
+                        <td>{e.studentId}</td>
+                        <td>{e.name}</td>
+                        <td>{e.email}</td>
+                        <td><input type="text" name="grade" value={e.grade} onChange={(event) => onGradeChange(event, e.enrollmentId)} /></td>
+                        <DialogActions>
+                            <Button onClick={onSave}>Update</Button>
+                        </DialogActions>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     );
 }
