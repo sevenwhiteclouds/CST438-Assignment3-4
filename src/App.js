c216c8e40191e825548db6c9c52b3a3495543591
sevenwhiteclouds <kemaldonado@csumb.edu>
2024-04-14 21:50:51 -0700

    add assignment 7 files

--- src/App.js
+++ src/App.js
@@ -1,74 +1,37 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UsersView from './components/admin/UsersView';
-import CoursesView from './components/admin/CoursesView';
-import SectionsView from './components/admin/SectionsView';
-import {AdminHome, AdminLayout} from './components/admin/AdminLayout';
-import {StudentLayout, StudentHome} from './components/student/StudentLayout';
-import ScheduleView from './components/student/ScheduleView';
-import Transcript from './components/student/Transcript';
-import StudentAssignmentsView from './components/student/AssignmentsStudentView';
-import InstructorLayout from './components/instructor/InstructorLayout';
-import InstructorHome from './components/instructor/InstructorHome';
-import AssignmentsView from './components/instructor/AssignmentsView';
-import EnrollmentsView from './components/instructor/EnrollmentsView';
-import InstructorSectionsView from './components/instructor/InstructorSectionsView';
-import CourseEnroll from "./components/student/CourseEnroll";
+import React, {useState } from 'react';
+import {AdminRouter} from './components/admin/AdminLayout';
+import {StudentRouter} from './components/student/StudentLayout';
+import {InstructorRouter} from './components/instructor/InstructorLayout';
+import Login from './Login';
 
 
 function App() {
 
-  // change to INSTRUCTOR or STUDENT for testing.  
-  // when login is implemented, the user type will come from the logged in user's ROLE.
+  const[isAuthenticated, setAuth] = useState(false);
+  const[userType, setUserType] = useState('');
 
-  const userType = 'INSTRUCTOR'; // change to INSTRUCTOR or STUDENT for testing.
+  const logout = () => {
+    setAuth(false);
+    sessionStorage.removeItem("jwt");
+  }
 
-  if (userType==='ADMIN') {
-    return (
-      <div className="App">
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<AdminLayout />}>
-              <Route index element={<AdminHome />} />
-              <Route path="users" element={<UsersView />} />
-              <Route path="courses" element={<CoursesView />} />
-              <Route path="sections" element={<SectionsView />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    )
+  if (! isAuthenticated) {
+      return (
+        <Login setAuth={setAuth} setUserType={setUserType} />
+      );
+  } else if (userType==='ADMIN') {
+      return (
+        <AdminRouter logout={logout}/>
+      )
   } else if (userType==='STUDENT') {
-    return (
-      <div className="App">
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<StudentLayout />}>
-              <Route index element={<StudentHome />} />
-              <Route path="schedule" element={<ScheduleView />} />
-              <Route path="studentAssignments" element={<StudentAssignmentsView />} />
-              <Route path="transcript" element={<Transcript />} />
-              <Route path="addCourse" element={<CourseEnroll />}/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    )
+      return (
+        <StudentRouter logout={logout}/>
+      )
+
   } else if (userType==='INSTRUCTOR') {
     return (
-      <div className="App">
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<InstructorLayout />}>
-              <Route index element={<InstructorHome />} />
-              <Route path="assignments" element={<AssignmentsView />} />
-              <Route path="enrollments" element={<EnrollmentsView />} />
-              <Route path="sections" element={<InstructorSectionsView />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <InstructorRouter logout={logout} />
     )
 
   } else {
@@ -76,4 +39,5 @@ function App() {
 
   }
 }
+
 export default App;
